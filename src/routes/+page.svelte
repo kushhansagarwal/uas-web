a32d9ea9fdeb1be76bed8cd0b8d38daef0d9507a
Kush Agarwal <kushagarwal@ucla.edu>
2024-09-06 22:48:50 +0530

    removed sponsors again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,25 +25,7 @@
 
 <Hero {image}></Hero>
 <InlineGallery></InlineGallery>
-<div class="bg-white mt-8">
-	<div class="mx-auto max-w-7xl bg-white">
-		<div
-			class="grid grid-cols-1 gap-12 rounded-xl bg-white p-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3"
-		>
-			{#each Array(6)
-				.fill(0)
-				.map((_, n) => n) as n}
-				<div class="">
-					<img
-						src={`src/lib/assets/sponsors/sponsor-${n + 1}.svg`}
-						alt={`Sponsor ${n}`}
-						class="h-auto w-full p-4"
-					/>
-				</div>
-			{/each}
-		</div>
-	</div>
-</div>
+
 <Sec1></Sec1>
 <Sec2></Sec2>
 <Sec3></Sec3>
