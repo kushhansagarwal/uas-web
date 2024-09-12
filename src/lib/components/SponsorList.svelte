a7d564bdbe16a1b36f6db54d7cfc995b1c078c9c
Kush Agarwal <kushagarwal@ucla.edu>
2024-09-12 20:02:50 +0530

    updated responsivness

--- src/lib/components/SponsorList.svelte
+++ src/lib/components/SponsorList.svelte
@@ -8,7 +8,7 @@
 	import solidworks from '$lib/assets/sponsors/solidworks.svg';
 	import boeing from '$lib/assets/sponsors/boeing.svg';
 
-	const sponsors = [aiaa, northrop, holybro, fetch, innovationlab, uclaalum, solidworks, boeing];
+	const sponsors = [fetch, innovationlab, holybro, aiaa, uclaalum, solidworks, boeing, northrop];
 </script>
 
 <div class="mx-auto max-w-7xl px-10 pt-10 text-center">
@@ -17,14 +17,19 @@
 		Thank you to our generous sponsors!
 	</p>
 	<p class="mt-6 text-lg leading-8 text-gray-300">
-		Our sponsors help us with funding and resources to continue our research and development. To learn more about our sponsors and to become a sponsor, visit our
+		Our sponsors help us with funding and resources to continue our research and development. To
+		learn more about our sponsors and to become a sponsor, visit our
 		<a href="/sponsorships" class="text-indigo-400">sponsorship page</a>.
 	</p>
 	<div
-		class="grid grid-cols-4 gap-20 p-10 opacity-100 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4"
+		class="grid grid-cols-2 gap-10 p-10 xs:gap-20 sm:grid-cols-3 sm:gap-10 md:gap-20 lg:grid-cols-4 lg:gap-20"
 	>
 		{#each sponsors as sponsor}
-			<img src={sponsor} alt={sponsor} />
+			<img
+				class="transform opacity-80 transition-transform duration-300 hover:scale-105 hover:opacity-100"
+				src={sponsor}
+				alt={sponsor}
+			/>
 		{/each}
 	</div>
 </div>
---
+++ src/routes/research/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import logo from '$lib/assets/logo.svg';
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen">
+
+    <div class="flex flex-col items-center justify-center">
+        <img src={logo} alt="logo" class="w-24 mb-10 opacity-60" />
+        <h1 class="text-2xl font-bold">Coming Soon...</h1>
+		<p class="text-gray-500 text-xl">We're just running checks and spinning props. This page will take flight soon!</p>
+	</div>
+</div>
